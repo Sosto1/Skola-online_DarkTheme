b8c58dfd36788fa5e93e991cb6a761079297ee6b
Sosto1 <sosty.mat.442@gmail.com>
2022-01-08 14:50:28 +0100

    updated github

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let img2 = document.getElementById("calendarPart_kalendar_500");
 if(img2 != null){img2.remove();}
 
 if (window.location.href.includes('RychlyPrehled')){
-    document.querySelector('.contact').innerHTML += '<br><span class="labInfo"></span><br><span class="btnSimulace"><a>Simulovat známky</a></span><span id="rfrsh" style="display: none;">X</span><br><br><a href="https://github.com/jakubhyza/skola-online-prumer-znamek" target="_blank">GitHub Dark theme</a>';
+    document.querySelector('.contact').innerHTML += '<br><span class="labInfo"></span><br><span class="btnSimulace"><a>Simulovat známky</a></span><span id="rfrsh" style="display: none;">X</span><br><br><a href="https://github.com/Sosto1/Skola-online_DarkTheme" target="_blank">GitHub Dark theme</a>';
 	
 
 }
